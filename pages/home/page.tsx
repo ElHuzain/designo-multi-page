bda6470534febaf45e563d0621f87a1472c22810
Ahmed Huzain <a.huzain98@gmail.com>
2024-01-15 23:52:39 +0200

    deployment

--- pages/home/page.tsx
+++ pages/home/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      Hey, I'm a page in PAGES!
+      {"Hey, I'm a page in PAGES!"}
       <Button>LEARN MORE</Button>
       <Button>GET IN TOUCH</Button>
     </div>
