21b94aa91e76849b7c1479d124506553b13ac170
Ahmed Huzain <a.huzain98@gmail.com>
2024-01-23 22:17:35 +0200

    style adjustments

--- components/pages/contact-us/components/contactForm.tsx
+++ components/pages/contact-us/components/contactForm.tsx
@@ -17,6 +17,7 @@ import {
     FormLabel,
     FormMessage,
 } from "@/components/ui/form"
+import useGetScreenWidth from '@/hooks/useGetScreenWidth';
 
 type errorElementType = {
     condition: any
@@ -33,6 +34,7 @@ const ErrorElement = ({ condition, msg }: errorElementType) => {
 const ContactForm = () => {
 
     const form = useForm();
+    const { screenWidth } = useGetScreenWidth();
 
     const onSubmit = (data: any) => {
         console.log(data)
@@ -42,17 +44,23 @@ const ContactForm = () => {
 
     return (
         <MaxWidthContainer bottomMargin={true} expands={true}>
-            <section className="py-[71px] px-5 overflow-hidden lg:px-[95px] relative sm:px-[58px] bg-primary text-white flex flex-col lg:flex-row gap-12 lg:gap-4 lg:justify-between sm:rounded-[15px]    ">
+            <section className="py-[71px] overflow-hidden lg:px-[95px] relative sm:px-[58px] bg-primary text-white flex flex-col lg:flex-row gap-12 lg:gap-4 lg:justify-between sm:rounded-[15px]    ">
 
-                <img alt='background-decoration' src='/assets/contact/desktop/bg-pattern-hero-desktop.svg' className="absolute top-[-90px] left-[-90px] lg:bottom-0 lg:left-0 lg:w-[57%] object-cover" />
 
-                <div className="text-center max-w-[635px] flex flex-col gap-8 justify-center sm:text-left">
+                {
+                    screenWidth === 'mobile'
+                        ? <img alt='background-decoration' src='/assets/contact/mobile/bg-pattern-hero-contact-mobile.svg' className="absolute w-full top-0" />
+                        : <img alt='background-decoration' src='/assets/contact/desktop/bg-pattern-hero-desktop.svg' className="absolute top-[-90px] left-[-90px] lg:bottom-0 lg:left-0 lg:w-[57%] object-cover" />
+
+                }
+
+                <div className="text-center max-w-[635px] flex flex-col px-5 gap-8 justify-center sm:text-left">
                     <h1 className="text-[32px] sm:text-[48px] font-[400]">Contact Us</h1>
                     <p className="font-[300] max-w-[440px]">{"Ready to take it to the next level? Let’s talk about your project or idea and find out how we can help your business grow. If you are looking for unique digital experiences that’s relatable to your users, drop us a line."}</p>
                 </div>
 
                 <Form {...form}>
-                    <form onSubmit={form.handleSubmit(onSubmit)} className="gap-8 lg:gap-[25px] w-full lg:max-w-[380px] flex flex-col">
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="gap-8 px-5 lg:gap-[25px] w-full lg:max-w-[380px] flex flex-col">
 
                         <FormItem className="relative">
                             <FormControl>
